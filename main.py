f61a8e60f2203194349585ad39fc5addb4b66232
M4recki <111280515+M4recki@users.noreply.github.com>
2023-06-06 16:55:45 +0200

    Adding the first files needed to complete the project, as well as starting the first work on the appearance of the website

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, request
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_gravatar import Gravatar
+from functools import wraps
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, ForeignKeyConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+import psycopg2
+
+app = Flask(__name__)
