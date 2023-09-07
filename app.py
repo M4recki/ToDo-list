da9e1e62ea2b7bd938c02c7be2be06fca95ef028
M4recki <111280515+M4recki@users.noreply.github.com>
2023-09-07 19:04:00 +0200

    Removed unnecessary condition if __name__ == "__main__" due to application running in a different file.

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('ToDoList_Database')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-if __name__ == "__main__":
-    app.run()
